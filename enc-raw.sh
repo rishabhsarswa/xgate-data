861f448996684635beed5c5caab699fedd2bfc6d
Rishabh Sarswa <rishabhsarswa@gmail.com>
2026-07-10 17:08:44 +0530

    mark update auto

---
+++ enc-raw.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+
+src_folder="$1"
+
+
+#src_folder="/home/rishu/Downloads"
+
+
+find "$src_folder" -type f -print0 | while IFS= read -r -d '' f; do
+    #echo "$f"
+    #dirname "$f"
+    #realpath --relative-to="$src_folder" "$f"
+    enc_fpt="./encrypted$f"
+    mkdir -p "$enc_fpt"
+    
+    
+		# echo -ne "\r\033[K"
+		  # Print new text on the same line
+		#  echo -ne "$f"
+    
+	if [ ! -f "$enc_fpt/stat.txt" ]; then
+		cur_stat=$(stat -c %y "$f")
+		#echo ""
+		echo "$enc_fpt"
+	    acrypter encrypt cry15360 "$f" "$enc_fpt/tmp.acrypt"
+	    mv "$enc_fpt/tmp.acrypt" "$enc_fpt/file.acrypt"
+	    
+	    echo "$cur_stat" > "$enc_fpt/stat.txt"
+	    
+	else
+		
+		cur_stat=$(stat -c %y "$f")
+		last_stat=$(cat "$enc_fpt/stat.txt")
+		if [[ "$cur_stat" != "$last_stat" ]]
+		then
+			#echo ""
+			echo "$enc_fpt"
+		    acrypter encrypt cry15360 "$f" "$enc_fpt/tmp.acrypt"
+		    mv "$enc_fpt/tmp.acrypt" "$enc_fpt/file.acrypt"
+		    
+		    echo "$cur_stat" > "$enc_fpt/stat.txt"
+		
+		fi
+	
+	
+	fi
+
+    
+done
+
+#echo ""
+
+echo "encryption done $src_folder"
+
---
+++ enc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+
+tar -cf "xgate-src.tar.xz" /home/rishu/awn/projects/xgate/xgate_src
+
+echo "source back done"
+
+./enc-raw.sh "/home/rishu/web/njb"
+./enc-raw.sh "/home/rishu/web/norm"
+#./enc-raw.sh "/home/rishu/xgate-data"
+
+find "." -type f -print0 | xargs -0 du -h | sort -h
+
+
+
